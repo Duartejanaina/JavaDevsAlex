a5e52a4bb6a5c7519e795f4bb5e621f03dbb145b
Duartejanaina <janainacduarte@gmail.com>
2021-12-01 11:33:01 -0200

    Criação do projeto.

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		/*Main apertar CTRL e barra- auto executável*/ 
+		System.out.println("Iniciando a programação, Hello World em Java");
+		System.out.println("POO");
+		System.out.println("Lógica de Programação");
+	}
+
+}
