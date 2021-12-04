d21931b33ad4daf0634027db10bc2e8ad2fe3e30
Duartejanaina <janainacduarte@gmail.com>
2021-12-04 14:16:40 -0200

    Char e if básicos

--- primeiraAula/src/curso/java/Curso.java
+++ primeiraAula/src/curso/java/Curso.java
@@ -10,9 +10,25 @@ public class Curso {
 	
 	double total = 0;
 	
-	char letra = 'F';
+	char letra = 'D';
 	
 	total = (nota1 + nota2 + nota3)/3;
+	
+	char genero1 = 'F';
+	char genero2 = 'X';
+	char genero3 = 'M';
+	
+	if (genero1 == 'F') {
+		System.out.println("Você se identifica com o gênero Feminino");
+	}
+	
+	if (genero2 == 'X') {
+		System.out.println("Você se identifica com gênero neutro");
+	}
+	
+	if (genero3 == 'M') {
+		System.out.println("Você se identifica com o gênero masculino");
+	}
 	
 	System.out.println(" O total é " + total + " código " + letra);
 	
