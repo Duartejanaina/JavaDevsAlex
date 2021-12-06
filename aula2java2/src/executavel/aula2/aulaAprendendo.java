ad1ec3339adcfd08cdc7a9a1ee4ca3fb1ca4c982
Duartejanaina <janainacduarte@gmail.com>
2021-12-06 18:57:06 -0200

    for com break

--- aula2java2/src/executavel/aula2/aulaAprendendo.java
+++ aula2java2/src/executavel/aula2/aulaAprendendo.java
@@ -2,6 +2,8 @@ package executavel.aula2;
 
 public class aulaAprendendo {
 	public static void main(String[]args){
+		
+		/* Do while*/
 		int numero = 0;
 		
 		do {
@@ -9,12 +11,22 @@ public class aulaAprendendo {
 			numero++;
 		} while (numero<=20);
 		
+		/*for*/
 		for (int numero1=0; numero1 <= 10; numero1++) {
 			System.out.println("O numero é: " +numero1);
 		}
 		
 		for (int numero2=10; numero2 >= 0; numero2--) {
 			System.out.println("O numero é: " +numero2);
+		}
+		
+		/*for com break*/
+		
+		for (int num=10; num >=0; num --) {
+			if (num == 7) {
+				System.out.println("Oba! Encontrei o número");
+				System.out.println("A partir de agora, o programa está encerrado.");
+			}
 		}
 	}
 
