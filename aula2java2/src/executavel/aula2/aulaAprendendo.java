55c195ab07109d4c8750eaaf1d855af615194be8
Duartejanaina <janainacduarte@gmail.com>
2021-12-06 17:13:52 -0200

    Dowhile

---
+++ aula2java2/src/executavel/aula2/aulaAprendendo.java
@@ -0,0 +1,15 @@
+package executavel.aula2;
+
+public class aulaAprendendo {
+	public static void main(String[]args){
+		int numero = 0;
+		
+		do {
+			System.out.println("O número atual é: " + numero);
+			numero++;
+		} while (numero<=10);
+		
+		
+	}
+
+}
