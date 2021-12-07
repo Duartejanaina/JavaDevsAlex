37bfd615e29163be02095152d8a92402db5aa607
Duartejanaina <janainacduarte@gmail.com>
2021-12-07 12:56:24 -0200

    Primeiro programa

---
+++ testeJogo/src/testeJogo/testeGameidade.java
@@ -0,0 +1,31 @@
+package testeJogo;
+
+import javax.swing.JOptionPane;
+
+public class testeGameidade {
+
+	public static void main(String[] args) {
+		String nome = JOptionPane.showInputDialog("Olá, gamer! Seja bem-vindo. Qual o seu nome?");
+		String idade = JOptionPane.showInputDialog("Digite a sua idade:");
+		double numeroIdade = Double.parseDouble(idade);
+		
+		if (numeroIdade >= 18) {
+			JOptionPane.showConfirmDialog(null,"Acesso Permitido, bem-vinda" + nome);
+			
+			String jogo = JOptionPane.showInputDialog("Selecione 1 para FarCry ou 2 para Free Fire");
+			double jogoescolha = Double.parseDouble(jogo);
+			if (jogoescolha == 1) {
+				JOptionPane.showConfirmDialog(null, "Welcome to The Jung");
+				
+			}
+			if (jogoescolha ==2) {
+				JOptionPane.showConfirmDialog(null, "Escolha sua skin");
+			}
+			
+			JOptionPane.showConfirmDialog(null, "Obrigada por jogar!");
+			
+		} else
+			JOptionPane.showConfirmDialog(null, "Acesso NEGADO, procure seu responsável");
+	}
+
+}
