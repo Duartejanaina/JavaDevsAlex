594b3d23668b8cc3c5e8f86760b1ad4591d62a6f
Duartejanaina <janainacduarte@gmail.com>
2021-12-13 15:48:28 -0200

    listas, removendo, acrescentando, resolvendo problemas de indexação

--- aula2java2/src/executavel/classes/aluno.java
+++ aula2java2/src/executavel/classes/aluno.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class aluno {
-	
+
 	private String nome;
 	private int idade;
 	private String dataNascimento;
@@ -14,24 +14,21 @@ public class aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieEscolar;
-	
+
 	private List<disciplina> disciplinas = new ArrayList<disciplina>();
-	
+
 	public void setDisciplinas(List<disciplina> disciplinas) {
 		this.disciplinas = disciplinas;
 	}
-	
+
 	public List<disciplina> getDisciplinas() {
 		return disciplinas;
 	}
-	
-	
-	
 
-	public void setNome (String nome) {
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -100,22 +97,20 @@ public class aluno {
 		this.serieEscolar = serieEscolar;
 	}
 
-
 	public double getmediaGlobal() {
-		
+
 		double somaNotas = 0.0;
-		
+
 		for (disciplina disciplina : disciplinas) {
 			somaNotas += disciplina.getNota();
-			
-			
+
 		}
-		
+
 		return somaNotas / disciplinas.size();
 	}
-	
+
 	public boolean getalunoAprovado() {
-		
+
 		double media = this.getmediaGlobal();
 		if (media >= 60) {
 			return true;
@@ -123,31 +118,33 @@ public class aluno {
 			return false;
 		}
 	}
-	
+
 	public String getalunoAprovado2() {
 		
 		double media = this.getmediaGlobal();
-		if (media >= 60) {
-			return "Aprovado";
+		if (media >= 50) {
+			if (media >= 60) {
+				return "Aprovado";
+			} else {
+				return "Recuperação";
+		} 
 		} else {
 			return "Reprovado";
 		}
 	}
+
 	@Override
 	public String toString() {
 		return "aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCPF=" + numeroCPF + ", nomeMae=" + nomeMae + ", dataMatricula="
-				+ dataMatricula + ", nomeEscola=" + nomeEscola + ", serieEscolar=" + serieEscolar  + getNome() + ", getIdade()="
-				+ getIdade() + ", getDataNascimento()=" + getDataNascimento() + ", getRegistroGeral()="
-				+ getRegistroGeral() + ", getNumeroCPF()=" + getNumeroCPF() + ", getNomeMae()=" + getNomeMae()
-				+ ", getDataMatricula()=" + getDataMatricula() + ", getNomeEscola()=" + getNomeEscola()
-				+ ", getSerieEscolar()=" + getSerieEscolar() + ", getmediaGlobal()=" + getmediaGlobal()
-				+ ", getalunoAprovado()=" + getalunoAprovado() + ", getalunoAprovado2()=" + getalunoAprovado2()
-				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
-				+ "]";
+				+ dataMatricula + ", nomeEscola=" + nomeEscola + ", serieEscolar=" + serieEscolar + getNome()
+				+ ", getIdade()=" + getIdade() + ", getDataNascimento()=" + getDataNascimento()
+				+ ", getRegistroGeral()=" + getRegistroGeral() + ", getNumeroCPF()=" + getNumeroCPF()
+				+ ", getNomeMae()=" + getNomeMae() + ", getDataMatricula()=" + getDataMatricula() + ", getNomeEscola()="
+				+ getNomeEscola() + ", getSerieEscolar()=" + getSerieEscolar() + ", getmediaGlobal()="
+				+ getmediaGlobal() + ", getalunoAprovado()=" + getalunoAprovado() + ", getalunoAprovado2()="
+				+ getalunoAprovado2() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
 	}
-
-	
-	
 
 }
