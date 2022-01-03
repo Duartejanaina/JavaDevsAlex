4cd1d56b8dbea8756e580def9d0650ecfd9e67e3
Duartejanaina <janainacduarte@gmail.com>
2022-01-03 17:48:41 -0200

    Tentando imprimir uma lista

--- consultorioInteligente/src/executavel/Consultorio/Consulta.java
+++ consultorioInteligente/src/executavel/Consultorio/Consulta.java
@@ -1,39 +1,43 @@
 package executavel.Consultorio;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JOptionPane;
-import javax.xml.crypto.Data;
-
 import consultorioInteligente.CadastroConsulta;
 import consultorioInteligente.Medico;
 import consultorioInteligente.Paciente;
 import consultorioInteligente.Pessoa;
 
 
+
+
 public class Consulta extends Pessoa {
 	
 	public static void main(String[] args) {
+		
+		String login = JOptionPane.showInputDialog("Login: ");
+		String senha = JOptionPane.showInputDialog("Senha: ");
+
+		Medico medico1 = new Medico();
+		medico1.setNome(JOptionPane.showInputDialog("Digite o nome do Médico"));
+		
+		List<Paciente> paciente = new ArrayList<Paciente>();
+		HashMap<String, List<Paciente>> maps  = new HashMap<String, List<Paciente>>();
+		
+		
+		for (int qtd = 0; qtd <= 1; qtd++) {
+
+			String nome = JOptionPane.showInputDialog("Nome do Paciente: ");
+
+			Paciente paciente1 = new Paciente();
+			paciente1.setNome(nome);
 			
-	Paciente paciente = new Paciente();
-	paciente.setConvenio("Unimed");
-	paciente.setEndereco("Alameda Jorge Luiz, 45, Santa Teresa, BH");
-	paciente.setIdCod(0001);
-	paciente.setCPF(9411);
-	paciente.setConvenio("Bradesco");
-	paciente.setNome("pedro");
-	
-	Medico medico = new Medico();
-	medico.setCrm(1223);
-	medico.setNome("Joao");
-	
-	CadastroConsulta cadastroConsulta = new CadastroConsulta ();
-	cadastroConsulta.setHorario("17:30");
-	
-	System.out.println(paciente.getNome());
-	
+		}
+		
+		System.out.println("Os pacientes " + paciente + "consultarão com a médica: "
+				+ medico1.getNome());
+		
 	}
 	
 
